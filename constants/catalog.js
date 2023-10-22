066b09354fbea1588a56427c26e84a330bcae16f
i <irinainani@gmail.com>
2023-10-22 21:40:17 +0300

    Witch Shop. Structure.

---
+++ constants/catalog.js
@@ -0,0 +1,113 @@
+const CATALOG = [
+  {
+    id: "el1",
+    name: "Flying Powder",
+    img: "img/catalog/potions/flying_powder.jpg",
+    price: "75",
+  },
+
+  {
+    id: "el2",
+    name: "Ghostly Potion",
+    img: "img/catalog/potions/ghostly_potion.jpg",
+    price: "75",
+  },
+
+  {
+    id: "el3",
+    name: "Hocus Pocus",
+    img: "img/catalog/potions/hocus_pocus.jpg",
+    price: "50",
+  },
+
+  {
+    id: "el4",
+    name: "Love Potion #9",
+    img: "img/catalog/potions/love_potion.jpg",
+    price: "100",
+  },
+
+  {
+    id: "el5",
+    name: "Shrinking Powder",
+    img: "img/catalog/potions/shrinking_powder.jpg",
+    price: "70",
+  },
+
+  {
+    id: "el6",
+    name: "Swamp Fog",
+    img: "img/catalog/potions/swamp_fog.jpg",
+    price: "60",
+  },
+
+  {
+    id: "el7",
+    name: "Alligator eyes",
+    img: "img/catalog/ingredients/alligator_eyes.jpg",
+    price: "10",
+  },
+
+  {
+    id: "el8",
+    name: "Bat Wings",
+    img: "img/catalog/ingredients/bat_wings.jpg",
+    price: "7",
+  },
+
+  {
+    id: "el9",
+    name: "Devil Dust",
+    img: "img/catalog/ingredients/devil_dust.jpg",
+    price: "30",
+  },
+
+  {
+    id: "el10",
+    name: "Dragon Dust",
+    img: "img/catalog/ingredients/dragon_dust.jpg",
+    price: "40",
+  },
+
+  {
+    id: "el11",
+    name: "Pixie Dust",
+    img: "img/catalog/ingredients/pixie_dust.jpg",
+    price: "25",
+  },
+
+  {
+    id: "el12",
+    name: "Ghost Whispers",
+    img: "img/catalog/ingredients/ghost_whispers.jpg",
+    price: "40",
+  },
+
+  {
+    id: "el13",
+    name: "Snake Scales",
+    img: "img/catalog/ingredients/snake_scales.jpg",
+    price: "35",
+  },
+
+  {
+    id: "el14",
+    name: "Spider Legs",
+    img: "img/catalog/ingredients/spider_legs.jpg",
+    price: "35",
+  },
+
+  {
+    id: "el15",
+    name: "Unicorn Horns",
+    img: "img/catalog/ingredients/unicorn_horns.jpg",
+    price: "40",
+  },
+
+  {
+    id: "el16",
+    name: "Witch Hazel",
+    img: "img/catalog/ingredients/witch_hazel.jpg",
+    price: "20",
+  },
+];
